674ac26946fa6fbffcea4a7fc7e6b8aa136e8d5f
Karim <benkiranemedkarim@gmail.com>
2025-03-04 15:50:12 +0000

    WIP

--- src/ch/epfl/rechor/PackedRange.java
+++ src/ch/epfl/rechor/PackedRange.java
@@ -5,6 +5,21 @@ public class PackedRange {
     }
 
     public static int pack(int startInclusive, int endExclusive) {
-        return 0;
+        Preconditions.checkArgument((startInclusive >> 24) == 0);
+        int length = endExclusive - startInclusive;
+        Preconditions.checkArgument((length >> 8) == 0);
+        return Bits32_24_8.pack(startInclusive, length);
+    }
+
+    public static int length(int interval) {
+        return Bits32_24_8.unpack8(interval);
+    }
+
+    public static int startInclusive(int interval) {
+        return Bits32_24_8.unpack24(interval);
+    }
+
+    public static int endExclusive(int interval) {
+        return startInclusive(interval) + length(interval);
     }
 }
